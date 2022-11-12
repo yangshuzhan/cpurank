7a6501d950a807cb8a6c04bef01afc7b9e42fe69
yangshuzhan <53788376+yangshuzhan@users.noreply.github.com>
2022-11-12 12:58:47 +0800

    Add files via upload

--- cpuworker.js
+++ cpuworker.js
@@ -1,5 +1,5 @@
 let sum=0,t1=Date.now();
-  for(let i=0;i<1000000;i++){
+  for(let i=0;i<10000000;i++){
     sum=((365/i)*Math.random())
     sum=((365%i)/Math.random())
     sum=((365*i)+Math.random())
@@ -7,4 +7,4 @@ let sum=0,t1=Date.now();
   }
   t1=Date.now()-t1;
   console.log(sum,t1*0.001+'s');
-  postMessage(t1);
+  postMessage(t1*0.1);
