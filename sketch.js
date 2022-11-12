3d5b8bbbd29ae15fc9f704806be423f5e51e034b
yangshuzhan <53788376+yangshuzhan@users.noreply.github.com>
2022-11-12 22:29:19 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
-let result
+let result,cores
 function setup() {
   createCanvas(400, 400);
-  console.log(window.navigator.hardwareConcurrency)
+  cores=window.navigator.hardwareConcurrency;
+  console.log()
   let workerList = [];
 result=[]
-for (let i = 0; i < window.navigator.hardwareConcurrency; i++) {
+for (let i = 0; i < cores; i++) {
   let newWorker = new Worker('cpuworker.js')
   newWorker.onmessage = function(e) {
     result.push(e.data);
@@ -13,20 +14,19 @@ for (let i = 0; i < window.navigator.hardwareConcurrency; i++) {
 }
 }
 
-function draw() {
+function draw(){
   background(220);
   //text('总时间:'+sumof(result)*0.001,10,30);
-  text('相当于'+300/sumof(result)*window.navigator.hardwareConcurrency/4+'个i3-9100f',10,30);
+  text('相当于'+300/sumof(result)*cores*cores/16+'个i3-9100f',10,30);
   for(let i=0;i<result.length;i++){
     text('core'+i,10,65+30*i);
-    rect(50, 50+30*i, result[i],20)
-
+    rect(50, 50+30*i, result[i],20);
   }
 }
 function sumof(arr){
   let sum=0;
   for(let i=0;i<arr.length;i++){
-    sum+=arr[i]
+    sum+=arr[i];
   }
-  return sum
+  return sum;
 }
