f767366047e55a4af249f893e606a00d69f6a778
yangshuzhan <53788376+yangshuzhan@users.noreply.github.com>
2022-11-12 12:33:01 +0800

    Add files via upload

---
+++ cpuworker.js
@@ -0,0 +1,10 @@
+let sum=0,t1=Date.now();
+  for(let i=0;i<1000000;i++){
+    sum=((365/i)*Math.random())
+    sum=((365%i)/Math.random())
+    sum=((365*i)+Math.random())
+    sum=((365-i)-Math.random())
+  }
+  t1=Date.now()-t1;
+  console.log(sum,t1*0.001+'s');
+  postMessage(t1);
---
+++ sketch.js
@@ -0,0 +1,32 @@
+let result
+function setup() {
+  createCanvas(400, 400);
+  console.log(window.navigator.hardwareConcurrency)
+  let workerList = [];
+result=[]
+for (let i = 0; i < window.navigator.hardwareConcurrency; i++) {
+  let newWorker = new Worker('cpuworker.js')
+  newWorker.onmessage = function(e) {
+    result.push(e.data);
+  }
+  workerList.push(newWorker);
+}
+}
+
+function draw() {
+  background(220);
+  //text('总时间:'+sumof(result)*0.001,10,30);
+  text('相当于'+460/sumof(result)+'个i3-9100f',10,30);
+  for(let i=0;i<result.length;i++){
+    text('core'+i,10,65+30*i);
+    rect(50, 50+30*i, result[i],20)
+
+  }
+}
+function sumof(arr){
+  let sum=0;
+  for(let i=0;i<arr.length;i++){
+    sum+=arr[i]
+  }
+  return sum
+}
