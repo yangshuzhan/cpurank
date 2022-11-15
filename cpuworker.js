aeb7aadd068271f2c4d48af3744cf7823266b195
yangshuzhan <53788376+yangshuzhan@users.noreply.github.com>
2022-11-15 14:54:55 +0800

    Update cpuworker.js

--- cpuworker.js
+++ cpuworker.js
@@ -1,11 +1,12 @@
 let sum,t1,t2;
 onmessage= function(e){
-  sum=0;
+  sum=0,t1=0,t2=0;
   t1=Date.now();
   for(let i=1;i<10000000;i++){
     sum+=1/(i*i)*Math.random();
   }
   t1=Date.now()-t1;
+  postMessage({array:[t1*0.1,t2*0.1]});
   t2=Date.now(),sum='this is a string';
   for(let i=1;i<10000000;i++){
     sum=(sum+i%10).substr(1,16);
